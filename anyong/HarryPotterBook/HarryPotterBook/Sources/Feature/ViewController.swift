8eb24355f34d184e8a0cf69503d219c293d1b247
ChoiAnYong <dksdyd78@naver.com>
2025-03-24 17:35:24 +0900

    feat: project setting

---
+++ anyong/HarryPotterBook/HarryPotterBook/Sources/Feature/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HarryPotterBook
+//
+//  Created by 최안용 on 3/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
