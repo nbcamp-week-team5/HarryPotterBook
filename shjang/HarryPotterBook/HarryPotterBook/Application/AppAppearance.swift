6f2ada227e9681369e777ab330896ef800eda93b
Seungho Jang <sjang1594@gmail.com>
2025-03-24 18:02:07 +0900

    fix: cherry pick
    (cherry picked from commit 885c8f4fd78252a71813f0b77c4f172aab4d4a1d)
    
    # Conflicts:
    #	shjang/HarryPotterBook/HarryPotterBook.xcodeproj/project.pbxproj
    #	shjang/HarryPotterBook/HarryPotterBook/Application/AppDelegate.swift

---
+++ shjang/HarryPotterBook/HarryPotterBook/Application/AppAppearance.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+final class AppAppearance {
+    static func setupAppearance() {
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.backgroundColor = UIColor.black
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        } else {
+            UINavigationBar.appearance().barTintColor = .black
+            UINavigationBar.appearance().tintColor = .white
+                        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        }
+    }
+}
--- shjang/HarryPotterBook/HarryPotterBook/Application/AppDelegate.swift
+++ shjang/HarryPotterBook/HarryPotterBook/Application/AppDelegate.swift
@@ -2,10 +2,6 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    let appDiContainer = AppDIContainer()
-    var appFlowCoordinator: AppFlowCoordinator?
-    var window: UIWindow?
-    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
