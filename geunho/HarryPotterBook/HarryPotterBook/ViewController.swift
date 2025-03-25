d40d0fd9394627eb66d7e9f3fa55387544b5671c
EllGHT <rmsgh1188@gmail.com>
2025-03-25 14:50:29 +0900

    feat: Level 1 뷰만 완성 (json X)

--- geunho/HarryPotterBook/HarryPotterBook/ViewController.swift
+++ geunho/HarryPotterBook/HarryPotterBook/ViewController.swift
@@ -6,27 +6,71 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    private let dataService = DataService()
+//    private let dataService = DataService()
     
-    func loadBooks() {
-        dataService.loadBooks { [weak self] result in
-            guard let self = self else { return }
-            
-            switch result {
-            case .success(let books):
-                
-                
-            case .failure(let error):
-            }
-        }
-    }
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Harry Potter and the Philosopher’s Stone"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
     
+    private lazy var seriesOrder: UIButton = {
+        let button = UIButton()
+        button.setTitle("1", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        button.backgroundColor = .systemBlue
+        button.layer.masksToBounds = true
+//        button.layer.cornerRadius = button.frame.width / 2
+        button.layer.cornerRadius = 20
+
+        return button
+    }()
+    
+//    func loadBooks() {
+//        dataService.loadBooks { [weak self] result in
+//            guard let self = self else { return }
+//            
+//            switch result {
+//            case .success(let books):
+//                
+//                
+//            case .failure(let error):
+//            }
+//        }
+//    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        configureLayout()
+    }
+    
+    private func configureLayout() {
+        view.backgroundColor = .white
+        
+        [titleLabel, seriesOrder].forEach { UILabel in
+            view.addSubview(UILabel)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20)
+            make.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(10)
+        }
+        
+        seriesOrder.snp.makeConstraints { make in
+            make.width.height.equalTo(40)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(titleLabel.snp.bottom).offset(16)
+        }
+        
     }
 
 
