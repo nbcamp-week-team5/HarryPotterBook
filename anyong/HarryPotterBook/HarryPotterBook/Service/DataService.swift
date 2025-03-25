9fcd997d64b0f53732004ce9cc3cd10d5966e54b
ChoiAnYong <dksdyd78@naver.com>
2025-03-25 10:20:13 +0900

    feat: DataService 구현

---
+++ anyong/HarryPotterBook/HarryPotterBook/Service/DataService.swift
@@ -0,0 +1,34 @@
+//
+//  DataService.swift
+//  HarryPotterBook
+//
+//  Created by 최안용 on 3/24/25.
+//
+
+
+import Foundation
+
+class DataService {
+    
+    enum DataError: Error {
+        case fileNotFound
+        case parsingFailed
+    }
+    
+    func loadBooks(completion: @escaping (Result<[Book], Error>) -> Void) {
+        guard let path = Bundle.main.path(forResource: "data", ofType: "json") else {
+            completion(.failure(DataError.fileNotFound))
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            let bookResponse = try JSONDecoder().decode(BookResponse.self, from: data)
+            let books = bookResponse.data.map { $0.attributes.toModel() }
+            completion(.success(books))
+        } catch {
+            print("🚨 JSON 파싱 에러 : \(error)")
+            completion(.failure(DataError.parsingFailed))
+        }
+    }
+}
