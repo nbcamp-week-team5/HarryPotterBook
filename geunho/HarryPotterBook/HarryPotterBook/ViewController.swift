ac4b6f9c3cfef7cabeafad2804b7f70fbb8b1219
EllGHT <rmsgh1188@gmail.com>
2025-03-25 19:27:49 +0900

    feat: Level 2 책 정보 영역 뷰

--- geunho/HarryPotterBook/HarryPotterBook/ViewController.swift
+++ geunho/HarryPotterBook/HarryPotterBook/ViewController.swift
@@ -10,9 +10,9 @@ import SnapKit
 
 final class ViewController: UIViewController {
     
-    
     private let dataService = DataService()
     
+    // MARK: - 제목 영역
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.text = "Harry Potter"
@@ -28,12 +28,124 @@ final class ViewController: UIViewController {
         button.titleLabel?.font = .systemFont(ofSize: 16)
         button.backgroundColor = .systemBlue
         button.layer.masksToBounds = true
-//        button.layer.cornerRadius = button.frame.width / 2
+        //        button.layer.cornerRadius = button.frame.width / 2
         button.layer.cornerRadius = 20
-
         return button
     }()
-
+    
+    
+    // MARK: - 책 정보 영역
+    // 메인 HStack
+    private lazy var hStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private lazy var bookImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "harrypotter1")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    // 책 상세 정보 VStack
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
+        return stackView
+    }()
+    private lazy var bookTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Harry Potter"
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        return label
+    }()
+    
+    // Author HStack
+    private lazy var authorStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
+        return stackView
+    }()
+    private lazy var author: UILabel = {
+        let label = UILabel()
+        label.text = "Author"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    private lazy var bookAuthor: UILabel = {
+        let label = UILabel()
+        label.text = "J. K. Rowling"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .darkGray
+        return label
+    }()
+    
+    // Released HStack
+    private lazy var releasedStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
+        return stackView
+    }()
+    private lazy var released: UILabel = {
+        let label = UILabel()
+        label.text = "Released"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        return label
+    }()
+    private lazy var bookReleased: UILabel = {
+        let label = UILabel()
+        label.text = "June 26, 1997"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .gray
+        return label
+    }()
+    
+    // Pages HStack
+    private lazy var pagesStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
+        return stackView
+    }()
+    private lazy var pages: UILabel = {
+        let label = UILabel()
+        label.text = "Pages"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        return label
+    }()
+    private lazy var bookPages: UILabel = {
+        let label = UILabel()
+        label.text = "100"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .gray
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -45,9 +157,29 @@ final class ViewController: UIViewController {
     private func configureLayout() {
         view.backgroundColor = .white
         
-        [titleLabel, seriesOrder].forEach { UILabel in
-            view.addSubview(UILabel)
+        [titleLabel, seriesOrder, hStackView].forEach {
+            view.addSubview($0)
         }
+        
+        [bookImageView, vStackView].forEach {
+            hStackView.addArrangedSubview($0)
+        }
+        
+        [bookTitle, authorStackView, releasedStackView, pagesStackView].forEach {
+            vStackView.addArrangedSubview($0)
+        }
+        
+        [author, bookAuthor].forEach {
+            authorStackView.addArrangedSubview($0)
+        }
+        
+        [released, bookReleased].forEach {
+            releasedStackView.addArrangedSubview($0)
+        }
+        [pages, bookPages].forEach {
+            pagesStackView.addArrangedSubview($0)
+        }
+        
         
         titleLabel.snp.makeConstraints { make in
             make.leading.equalToSuperview().inset(20)
@@ -60,6 +192,17 @@ final class ViewController: UIViewController {
             make.centerX.equalToSuperview()
             make.top.equalTo(titleLabel.snp.bottom).offset(16)
         }
+        
+        hStackView.snp.makeConstraints { make in
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).inset(20)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(70)
+            make.top.equalTo(seriesOrder.snp.bottom).offset(16)
+        }
+        
+        bookImageView.snp.makeConstraints { make in
+            make.width.equalTo(100)
+            make.height.equalTo(150)
+        }
     }
     
     func loadBooks() {
@@ -71,6 +214,10 @@ final class ViewController: UIViewController {
                 if let firstBook = books.first {
                     self.titleLabel.text = firstBook.title
                     self.seriesOrder.setTitle("1", for: .normal)
+                    self.bookTitle.text = firstBook.title
+                    self.bookAuthor.text = firstBook.author
+                    self.bookReleased.text = changeDateFormat(firstBook.releaseDate)
+                    self.bookPages.text = String(firstBook.pages)
                 }
             case .failure(let error):
                 print("에러: \(error)")
@@ -79,3 +226,6 @@ final class ViewController: UIViewController {
     }
 }
 
+#Preview {
+    ViewController()
+}
