7229c9b6ffba52dc4736d4d91f5a610e67a3e60a
EllGHT <rmsgh1188@gmail.com>
2025-03-25 14:06:57 +0900

    feat: DataService 추가

--- geunho/HarryPotterBook/HarryPotterBook/ViewController.swift
+++ geunho/HarryPotterBook/HarryPotterBook/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let dataService = DataService()
+    
+    func loadBooks() {
+        dataService.loadBooks { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let books):
+                
+                
+            case .failure(let error):
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +32,6 @@ class ViewController: UIViewController {
 
 }
 
+#Preview {
+    ViewController()
+}
