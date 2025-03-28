022baf95442317f34a491651a97444fbc1c33823
ChoiAnYong <dksdyd78@naver.com>
2025-03-28 17:34:08 +0900

    feat: 더보기 기능 구현

--- anyong/HarryPotterBook/HarryPotterBook/Presentation/View/BookDetailInfoSection.swift
+++ anyong/HarryPotterBook/HarryPotterBook/Presentation/View/BookDetailInfoSection.swift
@@ -10,14 +10,25 @@ import UIKit
 import SnapKit
 import Then
 
+protocol BookDetailInfoSectionDelegate: AnyObject {
+    func didTapExpendButton(_ expeded: Bool)
+}
+
 final class BookDetailInfoSection: UIView {
     private let dedicationTitleLabel = UILabel()
     private let dedicationLabel = UILabel()
     private let summaryTitleLabel = UILabel()
     private let summaryLabel = UILabel()
+    private let expendButton = UIButton(type: .system)
     
     private let dedicationStackView = UIStackView()
     private let summaryStackView = UIStackView()
+    
+    private var summary: String?
+    private var truncatedSummary: String?
+    private var isExpanded = false
+    
+    weak var delegate: BookDetailInfoSectionDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -56,6 +67,13 @@ final class BookDetailInfoSection: UIView {
             $0.numberOfLines = 0
         }
         
+        expendButton.do {
+            $0.setTitle(isExpanded ? "접기" : "더보기", for: .normal)
+            $0.titleLabel?.font = .systemFont(ofSize: 14)
+            $0.titleLabel?.textColor = .systemBlue
+            $0.addTarget(self, action: #selector(expandButtonTapped), for: .touchUpInside)
+        }
+        
         dedicationStackView.do {
             $0.axis = .vertical
             $0.spacing = 8
@@ -72,7 +90,7 @@ final class BookDetailInfoSection: UIView {
     private func setUI() {
         dedicationStackView.addArrangedSubViews(dedicationTitleLabel, dedicationLabel)
         summaryStackView.addArrangedSubViews(summaryTitleLabel, summaryLabel)
-        addSubViews(dedicationStackView, summaryStackView)
+        addSubViews(dedicationStackView, summaryStackView, expendButton)
     }
     
     private func setLayout() {
@@ -82,14 +100,62 @@ final class BookDetailInfoSection: UIView {
         
         summaryStackView.snp.makeConstraints {
             $0.top.equalTo(dedicationStackView.snp.bottom).offset(24)
-            $0.horizontalEdges.bottom.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        expendButton.snp.makeConstraints {
+            $0.top.equalTo(summaryStackView.snp.bottom)
+            $0.trailing.bottom.equalToSuperview()
         }
     }
 }
 
 extension BookDetailInfoSection {
-    func configure(_ book: Book) {
+    func configure(_ book: Book, _ isExpanded: Bool) {
         dedicationLabel.text = book.dedication
-        summaryLabel.text = book.summary
+        checkSummaryEllipsis(book.summary)
+        
+        guard let truncatedSummary else {
+            summaryLabel.text = book.summary
+            expendButton.isHidden = true
+            
+            return
+        }
+        
+        self.isExpanded = isExpanded
+        expendButton.setTitle(isExpanded ? "접기" : "더보기", for: .normal)
+        summaryLabel.text = isExpanded ? book.summary : truncatedSummary
+    }
+    
+    private func checkSummaryEllipsis(_ summary: String) {
+        self.summary = summary
+        
+        guard summary.count >= 450 else { return }
+        
+        var charCount = 0
+        var truncatedSummary = ""
+        
+        for line in summary.components(separatedBy: "\n") {
+            if charCount + line.count > 450 {
+                let remaining = 450 - charCount
+                truncatedSummary += line.prefix(remaining) + "..."
+                break
+            }
+            truncatedSummary += line + "\n"
+            charCount += line.count + 1
+        }
+        
+        self.truncatedSummary = truncatedSummary.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
+
+extension BookDetailInfoSection {
+    @objc
+    private func expandButtonTapped() {
+        isExpanded.toggle()
+        guard let summary, let truncatedSummary else { return }
+        summaryLabel.text = isExpanded ? summary : truncatedSummary
+        expendButton.setTitle(isExpanded ? "접기" : "더보기", for: .normal)
+        delegate?.didTapExpendButton(isExpanded)
     }
 }
