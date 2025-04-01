50cd893e2ca1f6535c0f6bacc9dd3a7e33bd1937
EllGHT <rmsgh1188@gmail.com>
2025-04-01 15:05:17 +0900

    chore: DataService 위치 변경

--- geunho/HarryPotterBook/HarryPotterBook/Models/DataService.swift
+++ geunho/HarryPotterBook/HarryPotterBook/Models/DataService.swift
@@ -14,10 +14,8 @@ class DataService {
         case parsingFailed
     }
     
-    // @escaping으로 인해 completion의 클로저가 loadBooks 종료 후에도 실행 가능!
     func loadBooks(completion: @escaping (Result<[Book], Error>) -> Void) {
         guard let path = Bundle.main.path(forResource: "data", ofType: "json") else {
-            // dataService.loadBooks 안에 있는 completion: { } 실행
             completion(.failure(DataError.fileNotFound))
             return
         }
@@ -26,11 +24,9 @@ class DataService {
             let data = try Data(contentsOf: URL(fileURLWithPath: path))
             let bookResponse = try JSONDecoder().decode(BookResponse.self, from: data)
             let books = bookResponse.data.map { $0.attributes }
-            // dataService.loadBooks 안에 있는 completion: { } 실행
             completion(.success(books))
         } catch {
             print("🚨 JSON 파싱 에러 : \(error)")
-            // dataService.loadBooks 안에 있는 completion: { } 실행
             completion(.failure(DataError.parsingFailed))
         }
     }
