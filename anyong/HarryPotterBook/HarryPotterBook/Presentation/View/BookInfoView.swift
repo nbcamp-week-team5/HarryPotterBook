02211123256d689160746f947c0425fb2728e289
ChoiAnYong <dksdyd78@naver.com>
2025-04-01 15:51:56 +0900

    feat: Level6 구현

--- anyong/HarryPotterBook/HarryPotterBook/Presentation/View/BookInfoView.swift
+++ anyong/HarryPotterBook/HarryPotterBook/Presentation/View/BookInfoView.swift
@@ -10,14 +10,20 @@ import UIKit
 import Then
 import SnapKit
 
+protocol BookInfoViewDelegate: AnyObject {
+    func didTapOrderButton(_ sender: UIButton)
+}
+
 final class BookInfoView: UIView {
     private let titleLabel = UILabel()
-    private let orderButton = UIButton(type: .system)
     private let scrollView = UIScrollView()
     private let contentView = UIView()
+    private let buttonStackView = UIStackView()
     private let bookInfoSection = BookInfoSection()
-    let bookDeatilInfoSection = BookDetailInfoSection()
+    let bookDetailInfoSection = BookDetailInfoSection()
     private let bookChapterSection = BookChapterSection()
+    
+    weak var delegate: BookInfoViewDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -42,15 +48,10 @@ final class BookInfoView: UIView {
             $0.numberOfLines = 2
         }
         
-        orderButton.do {
-            $0.titleLabel?.font = .systemFont(ofSize: 16)
-            $0.titleLabel?.textAlignment = .center
-            $0.setTitleColor(.systemBlue, for: .normal)
-            $0.setTitleColor(.white, for: [.selected, .disabled])
-            $0.tintColor = .clear
-            $0.clipsToBounds = true
-            $0.layer.cornerRadius = 20
-            $0.addTarget(self, action: #selector(didTapOrderButton), for: .touchUpInside)
+        buttonStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 10
+            $0.distribution = .fillEqually
         }
         
         scrollView.do {
@@ -61,9 +62,9 @@ final class BookInfoView: UIView {
     }
     
     private func setUI() {
-        addSubViews(titleLabel, orderButton, scrollView)
+        addSubViews(titleLabel, buttonStackView, scrollView)
         scrollView.addSubview(contentView)
-        contentView.addSubViews(bookInfoSection, bookDeatilInfoSection, bookChapterSection)
+        contentView.addSubViews(bookInfoSection, bookDetailInfoSection, bookChapterSection)
     }
     
     private func setLayout() {
@@ -72,15 +73,15 @@ final class BookInfoView: UIView {
             $0.horizontalEdges.equalToSuperview().inset(20)
         }
         
-        orderButton.snp.makeConstraints {
+        buttonStackView.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(16)
-            $0.centerX.equalToSuperview()
-            $0.size.equalTo(40)
+            $0.centerX.equalToSuperview()                        
         }
         
         scrollView.snp.makeConstraints {
-            $0.top.equalTo(orderButton.snp.bottom).offset(8)
-            $0.horizontalEdges.bottom.equalToSuperview()
+            $0.top.equalTo(buttonStackView.snp.bottom).offset(18)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide)
+            $0.bottom.equalToSuperview()
         }
         
         contentView.snp.makeConstraints {
@@ -93,13 +94,13 @@ final class BookInfoView: UIView {
             $0.horizontalEdges.equalToSuperview().inset(20)
         }
         
-        bookDeatilInfoSection.snp.makeConstraints {
+        bookDetailInfoSection.snp.makeConstraints {
             $0.top.equalTo(bookInfoSection.snp.bottom).offset(24)
             $0.horizontalEdges.equalToSuperview().inset(20)
         }
         
         bookChapterSection.snp.makeConstraints {
-            $0.top.equalTo(bookDeatilInfoSection.snp.bottom).offset(24)
+            $0.top.equalTo(bookDetailInfoSection.snp.bottom).offset(24)
             $0.horizontalEdges.equalToSuperview().inset(20)
             $0.bottom.equalToSuperview()
         }
@@ -107,22 +108,60 @@ final class BookInfoView: UIView {
 }
 
 extension BookInfoView {
-    func configure(_ book: Book, _ num: Int, _ isExpanded: Bool) {
+    func configure(_ book: Book, _ num: Int, _ count: Int) {
         titleLabel.text = book.title
-        orderButton.isSelected = false
-        orderButton.setTitle("\(num)", for: .normal)
-        orderButton.backgroundColor = orderButton.isSelected ? .systemBlue : .systemGray5
+
         bookInfoSection.configure(book, num)
-        bookDeatilInfoSection.configure(book, isExpanded)
+        bookDetailInfoSection.configure(book)
         bookChapterSection.configure(book)
+        if buttonStackView.subviews.isEmpty {
+            for i in 0..<count {
+                buttonStackView.addArrangedSubview(makeOrderButton(i+1))
+            }
+        }
     }
 }
+
 extension BookInfoView {
+    private func makeOrderButton(_ num: Int) -> UIButton {
+        let button = UIButton(type: .system)
+        
+        button.do {
+            $0.setTitle("\(num)", for: .normal)
+            $0.titleLabel?.font = .systemFont(ofSize: 16)
+            $0.titleLabel?.textAlignment = .center
+            $0.setTitleColor(.systemBlue, for: .normal)
+            $0.setTitleColor(.white, for: [.selected, .disabled])
+            $0.tintColor = .clear
+            $0.clipsToBounds = true
+            $0.layer.cornerRadius = 18
+            
+            $0.isSelected = num == 1 ? true : false
+            $0.setTitle("\(num)", for: .normal)
+            $0.backgroundColor = $0.isSelected ? .systemBlue : .systemGray5
+            $0.addTarget(self, action: #selector(didTapOrderButton(_:)), for: .touchUpInside)
+        }
+        
+        button.snp.makeConstraints {
+            $0.size.equalTo(35)
+        }
+        
+        return button
+    }
+    
     @objc
     private func didTapOrderButton(_ sender: UIButton) {
+        buttonStackView.arrangedSubviews.forEach { view in
+            guard let button = view as? UIButton else { return }
+            button.isSelected = false
+            button.isEnabled = true
+            button.backgroundColor = .systemGray5
+        }
+        
         sender.isSelected = true
         sender.isEnabled = false
-            
-        sender.backgroundColor = sender.isSelected ? .systemBlue : .systemGray5
+        sender.backgroundColor = .systemBlue
+        
+        delegate?.didTapOrderButton(sender)
     }
 }
